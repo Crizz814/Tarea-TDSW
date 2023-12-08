ad242f3b3154bfb3448f295cea2c3f5d1af00844
cristobal <cristobal.gonzalez1901@alumnos.ubiobio.cl>
2023-12-07 22:28:52 -0300

    pruebas

--- src/queries/queryRandom.jsx
+++ src/queries/queryRandom.jsx
@@ -1,21 +1,21 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 
-export function useRandomQuery() {
-  return useQuery('RandomQuery', RandomQuery, {
+export const useRandomQuery = (id) => {
+  return useQuery(['RandomQuery', id], () => fetchRandomPerro(id), {
     retry: 0,
     refetchOnWindowFocus: false,
     refetchOnMount: false,
     keepPreviousData: false,
-    enabled: true
+    enabled: true,
   });
-}
+};
 
-export const RandomQuery = async () => {
-    const cliente = axios.create({
-        baseURL: "http://localhost:8000/api/",
-    });
+const fetchRandomPerro = async (id) => {
+  const cliente = axios.create({
+    baseURL: "http://localhost:8000/api/",
+  });
 
-    const { data } = await cliente.get("perro/perroRandom");
-    return data.perro;
-}
+  const { data } = await cliente.get('perro/perroRandom/${id}');
+  return data.perro;
+};
